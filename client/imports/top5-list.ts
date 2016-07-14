f8dcd9b0921e936eddd43a86a1af1f628970c54c
David Johansson <ddjohn@gmail.com>
2016-07-14 23:26:13 +0200

    temp

--- client/imports/top5-list.ts
+++ client/imports/top5-list.ts
@@ -3,10 +3,11 @@ console.log("Loading <top5-list.ts>...");
 /* Framework */
 import "reflect-metadata";
 import {Component} from "@angular/core";
-import { ROUTER_DIRECTIVES }  from '@angular/router';
+import {ROUTER_DIRECTIVES }  from '@angular/router';
 import {Mongo} from "meteor/mongo";
 import {MeteorComponent} from 'angular2-meteor';
 import {InjectUser} from 'angular2-meteor-accounts-ui';
+import {PaginationService, PaginatePipe, PaginationControlsCmp} from 'angular2-pagination';
 
 /* Stuff */
 import {Top5} from "/collections/top5.ts"; 
@@ -14,6 +15,7 @@ import {Top5Form} from "./top5-form.ts";
 
 @Component({
   selector: "top5-list",
+  viewProviders: [PaginationService],
   template: `
   <div *ngIf="user">
     <top5-form></top5-form>
@@ -30,15 +32,17 @@ import {Top5Form} from "./top5-form.ts";
       <th>Name</th>
       <th *ngIf="user">Actions</th>
     </tr>
-    <tr *ngFor="let i of top5">
+    <tr *ngFor="let i of top5 | paginate:{currentPage:1, itemsPerPage:pageSize, totalItems:30}">
       <td>{{i.category}}</td>
       <td *ngIf="user"><a [routerLink]="['/top5', i._id]">{{i.name}}</a></td>
       <td *ngIf="!user">{{i.name}}</td>
       <td *ngIf="user"><button (click)="remove(i)">X</button></td>
     </tr>
   </table>
+  <pagination-controls (change)="onPageChanged($event.page)"></pagination-controls>
   `,
-  directives: [Top5Form, ROUTER_DIRECTIVES],
+  directives: [Top5Form, ROUTER_DIRECTIVES, PaginationControlsCmp],
+  pipes: [PaginatePipe],
 })
 
 @InjectUser("user")
@@ -46,13 +50,23 @@ import {Top5Form} from "./top5-form.ts";
 export class Top5List extends MeteorComponent { 
    user: Mongo.user;
    top5: Mongo.Cursor<Top5>;
+   pageSize: number = 5;
+   nameOrder: number = 1;
+   curPage: ReactiveVar<number> = new ReactiveVar<number>(1);
 
    constructor() {
      super();
 
      console.log("main::constructor");
-     this.subscribe('top5', () => {
-       this.top5 = Top5.find();
+
+     let options = {
+       limit: this.pageSize,
+       skip: (this.curPage - 1) * this.pageSize,
+       sort: { name: this.nameOrder }
+     };
+
+     this.subscribe('top5s', options, () => {
+       this.top5 = Top5.find({}, {sort: {name:this.nameOrder}});
      }, true);
    }
 
@@ -66,5 +80,10 @@ export class Top5List extends MeteorComponent {
     } else {
       this.top5 = Top5.find();
     }
+  }
+
+ onPageChanged(page: number) {
+    console.log("page: " + page);
+    this.curPage.set(page);
   }
 }
--- server/top5-publish.ts
+++ server/top5-publish.ts
@@ -3,12 +3,16 @@ console.log("Loading <top5-publish.ts>...");
 /* Framework */
 import {Top5} from '../collections/top5.ts';
 import {Meteor} from 'meteor/meteor';
+import {Counts} from 'meteor/tmeasday:publish-counts';
  
-Meteor.publish('top5', function() {
-  console.log("publish...");
-  return Top5.find();
+Meteor.publish('top5s', function(options: Object) {
+  console.log("any-publish...");
+  Counts.publish(this, 'numberOfParties', Top5.find({}), { noReady: true });
+
+  return Top5.find({}, options);
 });
 
 Meteor.publish('top5', function(id: string) {
-  return Top5.find({_id, id});
+  console.log("one-publish...");
+  return Top5.find({_id: id}, {});
 });
